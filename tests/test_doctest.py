080b54b52a67899737d66062c4c075e6a641132d
Jason Ash <ash.jasont@gmail.com>
2021-01-16 14:23:17 -0800

    add test module for doctests

---
+++ tests/test_doctest.py
@@ -0,0 +1,23 @@
+"""Explicitly add doctests to the unittest test suite"""
+import doctest
+
+from blokus import point, shape
+
+
+MODULES = (point, shape)
+
+
+def load_tests(loader, tests, ignored):  # pylint: disable=unused-argument
+    """
+    This function allows unittest to discover doctests in the module.
+    It appears not to use the arguments (or do anything, really), but this is
+    used internally by unittest to "discover" the tests it needs to run.
+
+    References
+    ----------
+    https://stackoverflow.com/questions/5681330/
+    https://docs.python.org/2/library/unittest.html#load-tests-protocol
+    """
+    for module in MODULES:
+        tests.addTests(doctest.DocTestSuite(module))
+    return tests
