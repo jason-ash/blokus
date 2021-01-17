cd9b4fcef18e45bce9f3802b870d72351640e229
Jason Ash <ash.jasont@gmail.com>
2021-01-16 16:00:42 -0800

    add the rest of the shape constructors

--- blokus/shape.py
+++ blokus/shape.py
@@ -4,7 +4,7 @@ from typing import FrozenSet, NamedTuple, Optional
 from blokus.point import Point
 
 
-class Shape(NamedTuple):
+class Shape(NamedTuple):  # pylint: disable=too-many-public-methods
     """Container for the points that make up a board piece (shape)"""
 
     origin: Point
@@ -68,7 +68,8 @@ class Shape(NamedTuple):
 
     @classmethod
     def I3(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
-        """Return the I3 shape.
+        """
+        Return the I3 shape.
 
         [X][ ][ ]
         """
@@ -174,8 +175,27 @@ class Shape(NamedTuple):
         return Shape(origin=origin, points=frozenset(points))
 
     @classmethod
+    def F(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the F shape.
+
+           [ ]
+           [ ][ ]
+        [X][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 1, y=origin.y + 1),
+            Point(x=origin.x + 2, y=origin.y + 1),
+            Point(x=origin.x + 1, y=origin.y + 2),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
     def I5(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
-        """Return the I5 shape.
+        """
+        Return the I5 shape.
 
         [X][ ][ ][ ][ ]
         """
@@ -189,18 +209,158 @@ class Shape(NamedTuple):
         return Shape(origin=origin, points=frozenset(points))
 
     @classmethod
+    def L5(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the L5 shape.
+
+        [ ]
+        [X][ ][ ][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x, y=origin.y + 1),
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 2, y=origin.y),
+            Point(x=origin.x + 3, y=origin.y),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def N(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the N shape.
+
+              [ ][ ]
+        [X][ ][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 2, y=origin.y),
+            Point(x=origin.x + 2, y=origin.y + 1),
+            Point(x=origin.x + 3, y=origin.y + 1),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def P(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the P shape.
+
+           [ ][ ]
+        [X][ ][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 1, y=origin.y + 1),
+            Point(x=origin.x + 2, y=origin.y),
+            Point(x=origin.x + 2, y=origin.y + 1),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
     def U(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
         """
-        Return a U shape.
+        Return the U shape.
 
         [ ]   [ ]
         [X][ ][ ]
         """
         points = [
-            Point(x=origin.x, y=origin.y),
+            origin,
             Point(x=origin.x, y=origin.y + 1),
             Point(x=origin.x + 1, y=origin.y),
             Point(x=origin.x + 2, y=origin.y),
             Point(x=origin.x + 2, y=origin.y + 1),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def V5(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the V5 shape.
+
+        [ ]
+        [ ]
+        [X][ ][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x, y=origin.y + 1),
+            Point(x=origin.x, y=origin.y + 2),
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 2, y=origin.y),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def W(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the W shape.
+
+              [ ]
+           [ ][ ]
+        [X][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 1, y=origin.y + 1),
+            Point(x=origin.x + 2, y=origin.y + 1),
+            Point(x=origin.x + 2, y=origin.y + 2),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def X(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the X shape.
+
+           [ ]
+        [ ][X][ ]
+           [ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x - 1, y=origin.y),
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x, y=origin.y - 1),
+            Point(x=origin.x, y=origin.y + 1),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def Y(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the Y shape.
+
+           [ ]
+        [X][ ][ ][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 1, y=origin.y + 1),
+            Point(x=origin.x + 2, y=origin.y),
+            Point(x=origin.x + 3, y=origin.y),
+        ]
+        return Shape(origin=origin, points=frozenset(points))
+
+    @classmethod
+    def Z5(cls, origin: Point) -> "Shape":  # pylint: disable=invalid-name
+        """
+        Return the Z5 shape.
+
+           [ ][ ]
+           [ ]
+        [X][ ]
+        """
+        points = [
+            origin,
+            Point(x=origin.x + 1, y=origin.y),
+            Point(x=origin.x + 1, y=origin.y + 1),
+            Point(x=origin.x + 1, y=origin.y + 1),
+            Point(x=origin.x + 2, y=origin.y + 2),
         ]
         return Shape(origin=origin, points=frozenset(points))
